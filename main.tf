a818203f25a8828f786600e58a00f556f9c7b259
Ronald Saenz <ronaldsaenz88@gmail.com>
2023-05-15 12:23:00 -0400

    Create DEV VPC - using modules

--- main.tf
+++ main.tf
@@ -14,8 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -35,7 +48,8 @@ module "blog_sg" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
   
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id              = module.vpc.public_subnets[0]
+
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
